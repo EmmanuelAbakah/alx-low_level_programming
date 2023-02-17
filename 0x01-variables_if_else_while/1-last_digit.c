a50ea27d235d80ed9cdd4a429c147a30e7f86dcd
EmmanuelAbakah <abakah.ema@gmail.com>
2023-02-17 12:37:57 -0800

    Task-1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,7 +6,7 @@
 /* betty style doc for function main goes there */
 /**
  * main -  Prints the last digit of a randomly generated number
-	and whether it is greater than 5, less than 6, or 0.
+ * and whether it is greater than 5, less than 6, or 0.
  *
  *Return: Always 0 (success)
  */
